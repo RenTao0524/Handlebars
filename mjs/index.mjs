22086905e384dfa211e207c77d192532f545781d
tao.ren <rentao0524@163.com>
2022-11-24 09:20:26 +0800

    some example

--- mjs/index.mjs
+++ mjs/index.mjs
@@ -1,7 +1,22 @@
-const template1 = '<p class="p1">{{firstname}} {{lastname}}</p>';
+// tempate1
+// const template1 = '<p class="p1">{{firstname}} {{lastname}}</p>';
+// const template1_info = {
+//     firstname: "Yehuda",
+//     lastname: "Katz",
+// };
+// const template1 =
+//   '<p class="p1">{{#with person}}{{firstname}} {{lastname}}{{/with}}</p>';
+// const template1_info = {
+//   person: {
+//     firstname: "Yehuda",
+//     lastname: "Katz",
+//   },
+// };
+
+const template1 =
+  '<ul class="people_list">{{#each people}}<li>{{this}}</li>{{/each}}</ul>';
 const template1_info = {
-  firstname: "Yehuda",
-  lastname: "Katz",
+  people: ["Yehuda Katz", "Alan Johnson", "Charles Jolley"],
 };
 
 function createTemplate1() {
@@ -9,7 +24,21 @@ function createTemplate1() {
   document.getElementById("template1").innerHTML = template(template1_info);
 }
 
+// template2
+function createTempalte2() {
+  const tempate = Handlebars.compile(
+    "<p>{{person.firstname}} {{person.lastname}}</p>"
+  );
+
+  document.getElementById("template2").innerHTML = tempate({
+    person: {
+      firstname: "Yehuda",
+      lastname: "Katz",
+    },
+  });
+}
 export default function createApp() {
   console.log("create app");
   createTemplate1();
+  createTempalte2();
 }
