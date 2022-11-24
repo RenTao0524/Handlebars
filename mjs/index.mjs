800351c862231612896f85a22089aabe80aef483
tao.ren <rentao0524@163.com>
2022-11-24 09:07:48 +0800

    render tempate1

--- mjs/index.mjs
+++ mjs/index.mjs
@@ -1,3 +1,15 @@
+const template1 = '<p class="p1">{{firstname}} {{lastname}}</p>';
+const template1_info = {
+  firstname: "Yehuda",
+  lastname: "Katz",
+};
+
+function createTemplate1() {
+  var template = Handlebars.compile(template1);
+  document.getElementById("template1").innerHTML = template(template1_info);
+}
+
 export default function createApp() {
   console.log("create app");
+  createTemplate1();
 }
